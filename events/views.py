c2a3fa354ddc830d7531b95822bc57c93fc91a45
Vijayashree2211 <vv20021122@gmail.com>
2025-02-17 19:51:02 +0530

    Event Description

---
+++ events/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from .models import Event
+from .serializers import EventSerializer
+
+class EventListCreate(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
